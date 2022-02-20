b29fee6727f07bf795c83707a21b64a3045ad8f6
Martin Yuan <myuan@fb.com>
2022-02-20 14:42:49 -0800

    Manual serialization methods to unblock the development
    ghstack-source-id: c6d1eecbbb44e3d2b71873f1491733254802769b
    Pull Request resolved: https://github.com/raziel/executorch/pull/6

--- core/tensor.h
+++ core/tensor.h
@@ -1,5 +1,15 @@
 #pragma once
 
+namespace c10 {
+struct alignas(1) qint8 {
+  using underlying = int8_t;
+  int8_t val_;
+  qint8() = default;
+  explicit qint8(int8_t val) : val_(val) {}
+};
+
+}
+
 namespace torch {
 namespace executor {
 // copied from pytorch/c10/core/ScalarType.h.
@@ -10,19 +20,21 @@ namespace executor {
   _(int16_t, Short) /* 2 */                              \
   _(int, Int) /* 3 */                                    \
   _(int64_t, Long) /* 4 */                               \
-  _(at::Half, Half) /* 5 */                              \
   _(float, Float) /* 6 */                                \
   _(double, Double) /* 7 */                              \
-  _(c10::complex<c10::Half>, ComplexHalf) /* 8 */        \
-  _(c10::complex<float>, ComplexFloat) /* 9 */           \
-  _(c10::complex<double>, ComplexDouble) /* 10 */        \
-  _(bool, Bool) /* 11 */                                 \
-  _(c10::qint8, QInt8) /* 12 */                          \
-  _(c10::quint8, QUInt8) /* 13 */                        \
-  _(c10::qint32, QInt32) /* 14 */                        \
-  _(at::BFloat16, BFloat16) /* 15 */                     \
-  _(c10::quint4x2, QUInt4x2) /* 16 */                    \
-  _(c10::quint2x4, QUInt2x4) /* 17 */
+  _(bool, Bool) /* 11 */
+
+// TODO: include necessary types from c10 (e.g. quantized)
+//  _(at::Half, Half) /* 5 */                              \
+//  _(c10::complex<c10::Half>, ComplexHalf) /* 8 */        \
+//  _(c10::complex<float>, ComplexFloat) /* 9 */           \
+//  _(c10::complex<double>, ComplexDouble) /* 10 */        \
+//  _(c10::qint8, QInt8) /* 12 */                          \
+//  _(c10::quint8, QUInt8) /* 13 */                        \
+//  _(c10::qint32, QInt32) /* 14 */                        \
+//  _(at::BFloat16, BFloat16) /* 15 */                     \
+//  _(c10::quint4x2, QUInt4x2) /* 16 */                    \
+//  _(c10::quint2x4, QUInt2x4) /* 17 */
 
 enum class ScalarType : int8_t {
 #define DEFINE_ENUM(_1, n) n,
@@ -32,16 +44,41 @@ enum class ScalarType : int8_t {
   NumOptions
 };
 
+constexpr uint16_t NumScalarTypes =
+    static_cast<uint16_t>(ScalarType::NumOptions);
+
+static constexpr uint8_t scalarTypeItemSizes[NumScalarTypes] = {
+#define SCALAR_TYPE_SIZE(T, name) sizeof(T),
+    AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(SCALAR_TYPE_SIZE)
+#undef SCALAR_TYPE_SIZE
+        0, // Undefined
+};
+
+
 // Tensor should be simple, but usable in operators
 // TODO: APIs common to at::Tensor
 struct Tensor {
   ScalarType type;
   void* data = nullptr;
-  int dim;
+  int dim = 0;
   int nbytes = 0;
-  int* sizes;
-  int* strides;
-  // Quantizer
+  int* sizes = nullptr;
+  int* strides = nullptr;
+  Tensor() {}
+  Tensor(ScalarType type, int dim, int* sizes, void* data=nullptr, int* strides = nullptr)
+  : dim(dim), sizes(sizes), type(type), data(data), strides(strides)
+  {
+    if (!data) {
+      nbytes = 0;
+      return;
+    }
+    nbytes = 1;
+    for (int i = 0; i < dim; ++i) {
+      nbytes *= sizes[i];
+    }
+    nbytes *= scalarTypeItemSizes[static_cast<uint16_t>(type)];
+  }
+  // TODO: Quantizer
 };
 
 } // namespace executor
--- test/test_executor.cpp
+++ test/test_executor.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <core/tensor.h>
 #include <core/value.h>
+#include <schema_generated.h>
+#include <unordered_map>
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
@@ -12,14 +14,206 @@ int main(int argc, char* argv[]) {
 // Tests go in torch::jit
 namespace torch {
 namespace executor {
+
+struct Serializer {
+  flatbuffers::Offset<executorch::Tensor> tensorToFB(
+      flatbuffers::FlatBufferBuilder& fbb,
+      const Value& ivalue) {
+    auto tensor = ivalue.toTensor();
+    // If there's no data in that tensor, the buffer_index is 0
+    int buffer_index = 0;
+    if (tensor->data) {
+      auto it = memoized_storage_map_.find(tensor->data);
+      if (it != memoized_storage_map_.end()) {
+        buffer_index = it->second;
+      }
+      else {
+        memoized_storage_map_[tensor->data] = ++buffer_index_;
+        tensor_data_.push_back(tensor);
+        buffer_index = buffer_index_;
+      }
+    }
+
+    std::vector<int> sizes{tensor->sizes[0], tensor->sizes[0] + tensor->dim};
+
+    return executorch::CreateTensorDirect(
+        fbb,
+        buffer_index,
+        static_cast<int8_t>(tensor->type),
+        0, // hard code storage offset for now. It's tensor's local storage offset, not related to serialization
+        &sizes,
+        0,
+        false, // requires grad
+        0);
+  }
+
+  flatbuffers::Offset<executorch::Value> valueToFB(flatbuffers::FlatBufferBuilder& fbb, const Value& ivalue) {
+    flatbuffers::Offset<void> offset = 0;
+    executorch::ValueUnion ivalue_type = executorch::ValueUnion::NONE;
+    if (ivalue.tag == Tag::Tensor) {
+
+      ivalue_type = executorch::ValueUnion::Tensor;
+      offset = tensorToFB(fbb, ivalue).Union();
+    } else {
+      // TODO: Support other types.
+      error_with_message("Type not supported yet.");
+    }
+    return CreateValue(fbb, ivalue_type, offset);
+  }
+
+  uint32_t storeValueAndGetIndex(
+      flatbuffers::FlatBufferBuilder& fbb,
+      const Value& ivalue) {
+    auto offset = valueToFB(fbb, ivalue);
+    uint32_t size = value_offsets_.size();
+    value_offsets_.push_back(offset);
+    return size;
+  }
+
+  void serializeValues(flatbuffers::FlatBufferBuilder& fbb, const std::vector<Value>& values) {
+    for (const auto& v : values) {
+      auto index = storeValueAndGetIndex(fbb, v);
+    }
+
+    // store buffers
+    // Buffer 0 is fixed for RW values
+    auto buffer_offset = executorch::CreateBuffer(
+        fbb,
+        fbb.CreateVector(
+            reinterpret_cast<const uint8_t*>(0),
+            0));
+    buffer_offsets_.push_back(buffer_offset);
+    for (auto td : tensor_data_) {
+      fbb.ForceVectorAlignment(
+          td->nbytes, sizeof(uint8_t), FLATBUFFERS_MAX_ALIGNMENT);
+      auto buffer_offset = executorch::CreateBuffer(
+          fbb,
+          fbb.CreateVector(
+              reinterpret_cast<const uint8_t*>(td->data),
+              td->nbytes));
+      buffer_offsets_.push_back(buffer_offset);
+    }
+  }
+
+  flatbuffers::DetachedBuffer serializeModule(flatbuffers::FlatBufferBuilder& fbb) {
+    // Prepare for a graph
+    // It has two operations, mul and add to finish
+    // z = a * x
+    // y = z * b
+
+    // values
+    // TODO: Add other types like IntList or BoolList
+    // Values: a, b, x, y and intermediate z (ax), all tensors
+    // Constant tensors a and b have data.
+    std::vector<Value> values;
+    auto a_sizes = new int[]{2, 2};
+    int a_data[4]{1, 2, 3, 4};
+    Tensor a(ScalarType::Int, 2, a_sizes, a_data);
+    values.emplace_back(&a);
+
+    auto b_sizes = new int[]{2, 2};
+    int b_data[4]{5, 6, 7, 8};
+    Tensor b(ScalarType::Int, 2, b_sizes, b_data);
+    values.emplace_back(&b);
+
+    // Rest of tensors (x, z, y) don't have data
+    auto x_sizes = new int[]{2, 2};
+    Tensor x(ScalarType::Int, 2, x_sizes);
+
+    auto y_sizes = new int[]{2, 2};
+    Tensor y(ScalarType::Int, 2, y_sizes);
+
+    auto z_sizes = new int[]{2, 2};
+    Tensor z(ScalarType::Int, 2, z_sizes);
+
+    values.emplace_back(&x);
+    values.emplace_back(&y);
+    values.emplace_back(&z);
+
+    serializeValues(fbb, values);
+
+    // operators
+    std::vector<flatbuffers::Offset<executorch::Operator>>
+        operator_vector;
+    operator_vector.push_back(executorch::CreateOperator(
+        fbb,
+        fbb.CreateSharedString("aten::mul"),
+        fbb.CreateSharedString("")));
+    operator_vector.push_back(executorch::CreateOperator(
+        fbb,
+        fbb.CreateSharedString("aten::add"),
+        fbb.CreateSharedString("")));
+
+    // 0: a,
+    // 1: b,
+    // 2: x,
+    // 3: y,
+    // 4: z
+
+    // Kernels
+    std::vector<flatbuffers::Offset<executorch::Kernel>> kernel_vector;
+    std::vector<int> op0_inputs{0, 2};
+    std::vector<int> op0_outputs{4};
+    kernel_vector.push_back(executorch::CreateKernelDirect(
+        fbb,
+        0, /* op index, 0 for mul */
+        &op0_inputs,
+        &op0_outputs
+        ));
+
+    std::vector<int> op1_inputs{4, 1};
+    std::vector<int> op1_outputs{3};
+    kernel_vector.push_back(executorch::CreateKernelDirect(
+        fbb,
+        1, /* op index, 0 for add */
+        &op1_inputs,
+        &op1_outputs
+        ));
+
+    std::vector<flatbuffers::Offset<executorch::Chain>> chain_vector;
+    std::vector<int> inputs{2}; // x. Q: would a and b counted inputs?
+    std::vector<int> outputs{3}; // y.
+    chain_vector.push_back(executorch::CreateChainDirect(
+        fbb,
+        &inputs,
+        &outputs,
+        &kernel_vector
+        ));
+
+
+    std::vector<flatbuffers::Offset<executorch::ExecutionPlan>> execution_plan_vector;
+    execution_plan_vector.push_back(executorch::CreateExecutionPlanDirect(
+        fbb,
+        &value_offsets_,
+        &inputs,
+        &outputs,
+        &chain_vector,
+        &operator_vector
+    ));
+
+    auto program_offset = executorch::CreateProgramDirect(
+        fbb,
+        1, /* version */
+        &execution_plan_vector,
+        &buffer_offsets_
+    );
+
+    fbb.Finish(program_offset);
+    return fbb.Release();
+  }
+
+  int buffer_index_ = 0; // 0 is hard-coded for RW data
+  std::vector<flatbuffers::Offset<executorch::Value>> value_offsets_;
+  std::vector<flatbuffers::Offset<executorch::Buffer>>
+      buffer_offsets_;
+  std::unordered_map<const void*, uint32_t> memoized_storage_map_;
+  std::vector<Tensor*> tensor_data_;
+};
+
 TEST(ExecutorTest, Tensor) {
-  Tensor a;
-  a.type = ScalarType::Int;
-  a.dim = 2;
-  a.sizes = new int[a.dim]{2, 2};
-  int a_data[4]{1, 2, 3, 4};
-  a.data = a_data;
-  a.nbytes = 2 * 2 * sizeof(int);
+  auto sizes = new int[]{2, 2};
+  int data[4]{1, 2, 3, 4};
+  Tensor a(ScalarType::Int, 2, sizes, data);
 
   auto data_p = static_cast<int*>(a.data);
   for (int i = 0; i < a.sizes[0]; ++i) {
@@ -30,18 +224,36 @@ TEST(ExecutorTest, Tensor) {
 }
 
 TEST(ExecutorTest, Value) {
-  Tensor a;
-  a.type = ScalarType::Int;
-  a.dim = 2;
-  a.sizes = new int[a.dim]{2, 2};
-  int a_data[4]{1, 2, 3, 4};
-  a.data = a_data;
-  a.nbytes = 2 * 2 * sizeof(int);
+  auto sizes = new int[]{2, 2};
+  int data[4]{1, 2, 3, 4};
+  Tensor a(ScalarType::Int, 2, sizes, data);
 
   Value v(&a);
   ASSERT_TRUE(v.isTensor());
   ASSERT_EQ(v.toTensor()->nbytes, 16);
 }
 
+TEST(ExecutorTest, Serialize) {
+  flatbuffers::FlatBufferBuilder fbb;
+  Serializer serializer;
+  auto buff = serializer.serializeModule(fbb);
+  auto program = executorch::GetProgram(buff.data());
+
+  ASSERT_EQ(program->execution_plan()->Length(), 1);
+  auto operators = program->execution_plan()->Get(0)->operators();
+  ASSERT_EQ(operators->Length(), 2);
+  ASSERT_EQ(operators->Get(1)->name()->str(), "aten::add");
+
+  auto values = program->execution_plan()->Get(0)->values();
+  ASSERT_EQ(values->Length(), 5);
+  auto b = values->Get(1)->val_as_Tensor();
+  ASSERT_EQ(b->sizes()->Length(), 2);
+  ASSERT_EQ(b->sizes()->Get(0), 2);
+
+  auto buffer = program->buffers()->GetMutableObject(b->buffer_index());
+  void* ptr = static_cast<void*>(buffer->mutable_data()->data());
+  auto d_ptr = static_cast<int*>(ptr);
+  ASSERT_EQ(d_ptr[3], 8);
+}
 } // namespace executor
 } // namespace torch
